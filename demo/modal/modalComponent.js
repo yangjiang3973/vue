67532e9bdf695e59661b8c2777c490637d22af55
IanJiang0817 <yang3973@gmail.com>
2021-01-12 14:55:13 +0800

    demo modal

--- demo/modal/modalComponent.js
+++ demo/modal/modalComponent.js
@@ -1,29 +1,22 @@
 export default {
   props: [],
   render() {
+    console.log(this.$slots);
     return (
       // <transition name="modal">
       <div class="modal-mask">
         <div class="modal-wrapper">
           <div class="modal-container">
-            {/* <div class="modal-header">
-                            <slot name="header">default header</slot>
-                        </div>
-                        <div class="modal-body">
-                            <slot name="body">default body</slot>
-                        </div> */}
-
+            <div class="modal-header">{this.$slots.header}</div>
+            <div class="modal-body">{this.$slots.body}</div>
             <div class="modal-footer">
-              {/* <slot name="footer"> */}
-              <slot>
-                default footer
-                <button
-                  class="modal-default-button"
-                  onClick={() => this.$emit("close")}
-                >
-                  OK
-                </button>
-              </slot>
+              {this.$slots.footer}
+              <button
+                class="modal-default-button"
+                onClick={() => this.$emit("close")}
+              >
+                OK
+              </button>
             </div>
           </div>
         </div>
